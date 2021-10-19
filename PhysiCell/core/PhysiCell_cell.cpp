b30e53c8b5afc5156daa05f0e4fb9671301496d4
Vincent Noël <contact@vincent-noel.fr>
2021-10-19 19:22:40 +0200

    Added PhysiBoSS

--- PhysiCell/core/PhysiCell_cell.cpp
+++ PhysiCell/core/PhysiCell_cell.cpp
@@ -77,6 +77,10 @@
 #include <algorithm>
 #include <iterator> 
 
+#ifdef ADDON_PHYSIBOSS
+#include "../addons/PhysiBoSS/src/maboss_intracellular.h"
+#endif
+
 namespace PhysiCell{
 	
 std::unordered_map<std::string,Cell_Definition*> cell_definitions_by_name; 
@@ -543,6 +547,8 @@ Cell* Cell::divide( )
 	
 	// child->set_phenotype( phenotype ); 
 	child->phenotype = phenotype; 
+	if (child->phenotype.intracellular)
+        child->phenotype.intracellular->start();
 	
 	return child;
 }
@@ -2172,6 +2178,28 @@ Cell_Definition* initialize_cell_definition_from_pugixml( pugi::xml_node cd_node
 		}
 	}	
 
+	// intracellular
+	
+	node = cd_node.child( "phenotype" );
+	node = node.child( "intracellular" ); 
+	if( node )
+	{
+		std::string model_type = node.attribute( "type" ).value(); 
+		
+#ifdef ADDON_PHYSIBOSS
+		if (model_type == "maboss") {
+			// If it has already be copied
+			if (pParent != NULL && pParent->phenotype.intracellular != NULL) {
+				pCD->phenotype.intracellular->initialize_intracellular_from_pugixml(node);
+				
+			// Otherwise we need to create a new one
+			} else {
+				MaBoSSIntracellular* pIntra = new MaBoSSIntracellular(node);
+				pCD->phenotype.intracellular = pIntra->getIntracellularModel();
+			}
+		}
+#endif
+	}
 	
 	// set up custom data 
 	node = cd_node.child( "custom_data" );
