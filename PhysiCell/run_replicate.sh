f6269b81d10dbf15ca57652cddd63208c1e7e4a0
Vincent Noël <vincent.noel@curie.fr>
2022-10-25 15:20:31 +0200

    Updated run_replicate.sh to accept optional folder argument

--- PhysiCell/run_replicate.sh
+++ PhysiCell/run_replicate.sh
@@ -2,5 +2,11 @@
 
 job_id=`printf "%02d" ${SLURM_PROCID}`
 
-echo ./COVID19 ./output_R${job_id}/config.xml
-exec ./COVID19 ./output_R${job_id}/config.xml
+if [ -z "$1" ]
+then
+    echo ./COVID19 ./output_R${job_id}/config.xml
+    exec ./COVID19 ./output_R${job_id}/config.xml
+else
+    echo ./COVID19 ./$1/output_R${job_id}/config.xml
+    exec ./COVID19 ./$1/output_R${job_id}/config.xml
+fi
